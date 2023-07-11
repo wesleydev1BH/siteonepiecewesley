09960a10a4974cb267dabf4b0ddade993616fcc6
Wesley Pereira <138945186+wesleydev1BH@users.noreply.github.com>
2023-07-11 20:31:15 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var menuIcon = document.querySelector('.menu-icon');
+var ul = document.querySelector('.ul');
+
+menuIcon.addEventListener('click',()=>{
+   if (ul.classList.contains('ativo')){
+    ul.classList.remove('ativo');
+    document.querySelector('.menu-icon img').src = 'imagens/menu.png';
+}else{
+ ul.classList.add('ativo');
+ document.querySelector('.menu-icon img').src = 'imagens/close.png'
+}   
+})
+
+var ajuda = document.querySelector('.ajuda-buton');
+ajuda.addEventListener('click',()=>{
+    alert  ('Mensagem enviada com sucesso!')
+})
